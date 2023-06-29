e7b4065ff85a9b8672d28954bc1568134aa5e694
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2023-06-29 13:56:45 +0200

    Upgrade nixpkgs

--- marp-cli.nix
+++ marp-cli.nix
@@ -9,10 +9,6 @@ mkYarnPackage {
     rev = "v2.3.0";
     sha256 = "zk52vYWdPU+U7GIXjoHaVP7ZRXWmVyL/DcOaR2dmfps=";
   };
-  packageResolutions = {
-    pug = "^3.0.2";
-    pug-runtime = "^3.0.1";
-  };
   buildPhase = ''
     export HOME=$(mktemp -d)
     yarn --offline build
